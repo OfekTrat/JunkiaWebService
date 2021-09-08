f3fe3759fa66f9d6b6a623da1691129fe9fdf90a
Ofek Trattner <ofekikotrat@gmail.com>
2021-09-08 11:25:35 +0300

    Changed gitignore and app

--- project/src/app.py
+++ project/src/app.py
@@ -1,4 +1,17 @@
 from flask import Flask, jsonify, request
-
+import mysql.connector as connector
+from finding import Finding
+from location import Location
+from db_communicator.mysql_communicator.mysql_communicator import MySQLCommunicator
 
 app = Flask(__name__)
+
+
+conn = connector.connect(
+    host="localhost",
+    user="root",
+    password="OfekT2021"
+)
+
+
+
