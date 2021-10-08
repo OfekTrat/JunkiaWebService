cf729f2aad6922c296dba41eb45292e6b5fbfaae
Ofek <ofekikotrat@gmail.com>
2021-10-08 15:34:35 +0300

    Created cloud executor, and added google cloud key

---
+++ src/db_communicators/mysql_communicator/mysql_executer/mysql_cloud_executor.py
@@ -0,0 +1,31 @@
+from typing import Tuple
+from pymysql.cursors import Cursor, DictCursor
+from pymysql.connections import Connection
+from google.cloud.sql.connector import connector
+from .iexecuter import IExecuter
+
+
+class MyCloudSQLExecuter(IExecuter):
+    def __init__(self, host: str, user: str, password: str):
+        self.__host = host
+        self.__user = user
+        self.__password = password
+
+    def __enter__(self) -> Tuple[Connection, Cursor]:
+        self.__conn = self._get_connection()
+        self.__cursor = self.__conn.cursor(DictCursor)
+        return self.__conn, self.__cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__cursor.close()
+        self.__conn.close()
+
+    def _get_connection(self) -> Connection:
+        return connector.connect(
+            "junkiapp:us-central1:junkia",
+            "pymysql",
+            user=self.__user,
+            password=self.__password,
+            db="junkia"
+        )
+
